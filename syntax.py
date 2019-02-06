c318423ad8594ce19713956f4fcec740a7aaed83
Roni <ronicse2011@gmail.com>
2019-02-06 10:33:42 +0600

    Commenting

--- syntax.py
+++ syntax.py
@@ -2,3 +2,11 @@
 
 if 5 > 2:
  	print("Five is grater than two")
+
+
+# Docstring for triple quote for single or multiple lines and # is for single line
+
+""" This is a
+multiline docstring """
+
+print("Hello World")
