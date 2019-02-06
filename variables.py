976975e6d0b95f9757b5f60741bf54522b488d47
Roni <ronicse2011@gmail.com>
2019-02-06 10:54:18 +0600

    Variables

---
+++ variables.py
@@ -0,0 +1,27 @@
+# Creating variables
+x = 6
+y = "string variable"
+print(x)
+print(y)
+
+# Type case easily just put value it will change autimacally type
+
+x = 7 # x is of type int
+x = "Roni" # x is of type str
+print(x)
+
+
+# output variables
+x = "Awesome"
+print("Python is " + x)
+
+# + is mathmetical operator when it is integer value
+x = 5
+y = 20
+print(x+y)
+
+# 
+x = 5
+y = "This is "
+z = y + x
+print(z)
