6bb5529af19ae84101d3eb181bdd1ffc1c855dd5
Roni <ronicse2011@gmail.com>
2019-02-07 10:51:51 +0600

    Python Numbers

---
+++ numbers.py
@@ -0,0 +1,22 @@
+# There are three numeric types in Python
+#int, float,, complex
+
+# int
+x = 1234
+
+print(x)
+
+# float
+y = .2e3
+z = 12.10
+
+print(y)
+print(z)
+
+# complex
+x = 2+5j
+y = 5j
+z = -2j
+print(x)
+print(y)
+print(z)
